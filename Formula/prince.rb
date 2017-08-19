1fb4db79059abc45e85db3ff26c01b8e9e00c7f5
Ben Damman <ben.damman@gmail.com>
2017-08-19 11:20:12 -0700

    Update prince.rb

--- Formula/prince.rb
+++ Formula/prince.rb
@@ -9,37 +9,4 @@ class Prince < Formula
     ENV["prefix"] = prefix
     system "./install.sh"
   end
-
-  def patches
-    # Drop the user input since we're providing the prefix
-    DATA
-  end
 end
-
-__END__
---- prince-9.0r5-macosx.orig/install.sh	2014-04-28 02:21:00.000000000 +0000
-+++ prince-9.0r5-macosx/install.sh	2014-05-16 14:44:07.000000000 +0000
-@@ -7,24 +7,10 @@
- VERSION="11.3"
- WEBSITE="http://www.princexml.com"
- 
--prefix=/usr/local
--
- base=`dirname $0`
- 
- cd "$base"
- 
--echo "$PRODUCT $VERSION"
--echo
--echo "Install directory"
--echo "    This is the directory in which $PRODUCT $VERSION will be installed."
--echo "    Press Enter to accept the default directory or enter an alternative."
--echo -n "    [$prefix]: "
--
--read input
--if [ ! -z "$input" ] ; then
--    prefix="$input"
--fi
--
- echo
- echo "Installing $PRODUCT $VERSION..."
